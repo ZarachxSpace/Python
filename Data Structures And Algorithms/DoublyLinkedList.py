39966c83dfd940324465e82925de09618610e86b
Zarach <14337179+ZarachxSpace@users.noreply.github.com>
2025-03-20 21:26:05 +0530

    fixed extension

---
+++ Data Structures And Algorithms/DoublyLinkedList.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.value = value
+        self.head = new_node
+        self.tail = new_node
+
+    def print_list(self):
+        temp = self.head
+        while self.head:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else: 
+            
