599a06fd7917296244797e0e3857517eabcb3001
ZarachxSpace <114337179+ZarachxSpace@users.noreply.github.com>
2026-04-05 18:53:44 +0530

    DFour

--- Python Projects/Basic/isPalindrome.py
+++ Python Projects/Basic/isPalindrome.py
@@ -1,16 +1,21 @@
 # def is_palindrome(s):
-#     pure_text = "".join(char for char in s if char.isalnum())
-#     reverse = pure_text[::-1]
-#     return pure_text.lower() == reverse.lower()
+#     pure_text = "".join(char.lower() for char in s if char.isalnum())
+#     return pure_text == pure_text[::-1]
+
 
 def is_palindrome(s):
-    reverse = ""
-    pure_text = "".join(char for char in s if char.isalnum())
-    for char in pure_text:
-        reverse = char + reverse
-    return (reverse.lower() == pure_text.lower())
+    pure_text = "".join(char.lower() for char in s if char.isalnum())
+    left, right = 0, len(pure_text) - 1
+    while left < right:
+        if pure_text[left] != pure_text[right]:
+            return False
+        
+        left += 1
+        right -= 1
+
+    return True
 
 
-s = "ttytt"
+s = "Racecar"
 print(is_palindrome(s))
 
